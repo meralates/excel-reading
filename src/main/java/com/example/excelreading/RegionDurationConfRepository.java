46f56253e455f639767b4fddcee6bacec0d9e9e9
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-04 14:57:35 +0300

    Initial commit

---
+++ src/main/java/com/example/excelreading/RegionDurationConfRepository.java
@@ -0,0 +1,4 @@
+package com.example.excelreading;
+
+public interface RegionDurationConfRepository {
+}
---
+++ src/main/java/com/example/excelreading/RegionDurationConfService.java
@@ -0,0 +1,4 @@
+package com.example.excelreading;
+
+public class RegionDurationConfService {
+}
